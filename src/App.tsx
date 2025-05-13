1bacb1237063cf2903be4fdf6363ebe7047f272e
joeally06 <62224411+joeally06@users.noreply.github.com>
2025-05-13 11:34:21 -0500

    Create Admin Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { News } from './pages/News';
 import { Events } from './pages/Events';
 import { Members } from './pages/Members';
 import { AdminLogin } from './pages/AdminLogin';
+import { AdminDashboard } from './pages/AdminDashboard';
 import ConferenceRegistration from './pages/ConferenceRegistration';
 import { AdminConferenceRegistrations } from './pages/AdminConferenceRegistrations';
 import { AdminConferenceSettings } from './pages/AdminConferenceSettings';
@@ -56,7 +57,9 @@ function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/conference-registration" element={<ConferenceRegistration />} />
               <Route path="/hall-of-fame-nomination" element={<HallOfFameNomination />} />
-              <Route path="/admin/login" element={<AdminLogin />} />              <Route path="/admin/conference-registrations" element={<AdminConferenceRegistrations />} />
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/admin/conference-registrations" element={<AdminConferenceRegistrations />} />
               <Route path="/admin/conference-settings" element={<AdminConferenceSettings />} />
               <Route path="/admin/test" element={<AdminTestComponent />} />
               <Route path="/admin/debug" element={<UserDebugComponent />} />
